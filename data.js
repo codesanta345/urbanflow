587ada529f8bdf761afb2152839006bd830c7a4e
shruti <singla.shruti2711@gmail.com>
2026-09-13 01:58:14 +0530

    Revert "Updated UrbanFlow frontend and traffic simulation"
    This reverts commit 839eab7e3585e0d97f355f181d4f77d6152e8ff9.

--- data.js
+++ data.js
@@ -52,12 +52,6 @@ const UrbanFlowData = (() => {
       roleTitle: COMMUTER_PROFILE.roleTitle,
       accessLevel: COMMUTER_PROFILE.accessLevel,
       permissions: COMMUTER_PROFILE.permissions
-    },
-    traffic_manager: {
-      role: 'traffic_manager',
-      roleTitle: 'Traffic Authority',
-      accessLevel: 'Traffic Operations',
-      permissions: 'Signal control, incident verification, live radar, emergency corridor'
     }
   };
 
